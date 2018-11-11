3edc8ac58469bbe25096b55450884f4b13879688
Michael Ingeman-Nielsen <michael@i-n.dk>
2018-11-11 23:48:58 +0100

    Integers and iteration

---
+++ iteration/repeat_test.go
@@ -0,0 +1,25 @@
+package iteration
+
+import "testing"
+import "fmt"
+
+func TestRepeat(t *testing.T)  {
+	repeated := Repeat("a", 7)
+	expected := "aaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("Expected '%s' but got '%s'", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B)  {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("z", 3)
+	fmt.Println(repeated)
+	// Output: zzz
+}
