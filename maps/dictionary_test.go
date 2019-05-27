7f0648da82c8997828631269e9c19dc35c1ba58b
Michael Ingeman-Nielsen <michael@i-n.dk>
2019-05-27 23:44:55 +0200

    Error handling from maps

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,18 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		assertError(t, err, ErrNotFound)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
@@ -16,5 +24,13 @@ func assertStrings(t *testing.T, got, want string) {
 
 	if got != want {
 		t.Errorf("got '%s' want '%s' given, '%s'", got, want, "test")
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error '%s', want '%s'", got, want)
 	}
 }
